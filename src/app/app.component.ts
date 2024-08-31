743e5345abc4d7f9ccec6762e034ca3b8e2003b0
Jeremy Guinn <jeremyguinn97@gmail.com>
2024-08-31 15:23:33 -0600

    Bring back native title bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,17 @@
-import { Component, HostListener, inject, Renderer2 } from '@angular/core';
-import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
-import { TitlebarComponent } from './layout/titlebar/titlebar.component';
 import { TextDiffComponent } from './components/text-diff/text-diff.component';
-
-const appWindow = getCurrentWebviewWindow();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TitlebarComponent, TextDiffComponent],
+  imports: [CommonModule, RouterOutlet, TextDiffComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private document = inject(DOCUMENT);
-  private renderer = inject(Renderer2);
-
   constructor() {}
-
-  @HostListener('window:resize')
-  async onResize(): Promise<void> {
-    if (await appWindow.isMaximized()) {
-      this.renderer.addClass(this.document.body, 'maximized');
-    } else {
-      this.renderer.removeClass(this.document.body, 'maximized');
-    }
-  }
 
   @HostListener('window:dragover', ['$event'])
   preventFileDrop(e: DragEvent): void {
