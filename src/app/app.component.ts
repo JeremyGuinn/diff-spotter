76947176c5fc3726c25ac7d20a53ae9254ba0b8a
Jeremy Guinn <jeremyguinn97@gmail.com>
2024-08-30 18:22:36 -0600

    Run linter and pretty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from '@tauri-apps/api/tauri';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  greetingMessage = "";
+  greetingMessage = '';
 
   greet(event: SubmitEvent, name: string): void {
     event.preventDefault();
 
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke<string>("greet", { name }).then((text) => {
+    invoke<string>('greet', { name }).then(text => {
       this.greetingMessage = text;
     });
   }
