a828f6fcd815800d4329022cd2239d0222479ea8
Jeremy Guinn <jeremyguinn97@gmail.com>
2024-09-08 07:59:31 -0600

    Positioning tweaks

--- src/app/views/new-diff/new-diff.component.ts
+++ src/app/views/new-diff/new-diff.component.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { DiffMethod } from '@app/services/diffs';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { remixFileFill, remixImageFill } from '@ng-icons/remixicon';
 
 @Component({
   selector: 'app-new-diff',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconComponent],
+  providers: [
+    provideIcons({
+      remixImageFill,
+      remixFileFill,
+    }),
+  ],
   templateUrl: './new-diff.component.html',
   styleUrl: './new-diff.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
