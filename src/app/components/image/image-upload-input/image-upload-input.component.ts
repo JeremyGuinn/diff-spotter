ea9ca777ed620fd7276830fa3e9f2cd7fb38b1c7
Jeremy Guinn <jeremyguinn97@gmail.com>
2024-09-09 20:22:00 -0600

    Fix image upload dropzone

--- src/app/components/image/image-upload-input/image-upload-input.component.ts
+++ src/app/components/image/image-upload-input/image-upload-input.component.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { remixImageFill } from '@ng-icons/remixicon';
 
@@ -18,6 +26,37 @@ import { remixImageFill } from '@ng-icons/remixicon';
 })
 export class ImageUploadInputComponent {
   @Output() fileUpload = new EventEmitter<File | null>();
+
+  @ViewChild('dropZone') dropZone: ElementRef<HTMLElement> | undefined;
+
+  @HostListener('dragenter', ['$event'])
+  addDragOverClass(e: DragEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log('drag enter');
+
+    this.dropZone?.nativeElement.classList.add('drag-over');
+  }
+
+  @HostListener('dragleave', ['$event'])
+  removeDragOverClass(e: DragEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+    this.dropZone?.nativeElement.classList.remove('drag-over');
+  }
+
+  @HostListener('dragover', ['$event'])
+  @HostListener('drop', ['$event'])
+  allowFileDrop(e: DragEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const file = e.dataTransfer?.files?.[0];
+    if (file) {
+      this.fileUpload.emit(file);
+    }
+  }
 
   handleFileUpload(files: FileList) {
     this.fileUpload.emit(files?.item(0) || null);
